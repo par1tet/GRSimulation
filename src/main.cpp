97fc8bfbef723804f04cc051323e31a9b6f4d9d6
par1tet <mankova486@gmail.com>
2025-06-13 10:53:35 +0300

    feat: create window

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,38 @@
-#include<iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <SOIL2/SOIL2.h>
+#include <iostream>
 
 int main(){
-    std::cout << "hello" << std::endl;
+    glfwInit();
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "GLSim", nullptr, nullptr);
+
+    if (window == nullptr){
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+        return -1;
+    }
+
+    int width, heigth;
+    glfwGetFramebufferSize(window, &width, &heigth);
+
+    glViewport(0,0, width, heigth);
+
+    while(!glfwWindowShouldClose(window)){
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+    
+    glfwTerminate();
     return 0;
 }
